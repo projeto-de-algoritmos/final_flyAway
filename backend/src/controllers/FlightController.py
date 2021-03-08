f3cf47d3250b7c0dc3b10696328a092e68598010
Ian Rocha <ianrocha.df@gmail.com>
2021-03-08 19:52:01 -0300

    Metodo GET dos Voos

--- backend/src/controllers/FlightController.py
+++ backend/src/controllers/FlightController.py
@@ -1,11 +1,29 @@
 from ..services.FlightService import FlightService
 from fastapi import HTTPException
+import requests
 
 class FlightController():
-    def __init__(self):
+    def __init__(self, currency, locale, originCountry, quotes, places):
         self.flightService = FlightService()
+        self.currency = currency
+        self.locale = locale
+        self.rootURL = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browseroutes/v1.0
+        self.originCountry = originCountry
+        self.quotes = []
+        self.places = []
 
-    async def getFlights(self):
+    async def getFlights(self, rootURL, currency, locale, rootURL, originCountry, sourceCountry, destinationCountry, date, returnDate):
+
+        url = rootURL + "/" + originCountry + "/" + currency + "/" + locale + "/" + sourceCountry + "/" destinationCountry + "/" + date.strftime("%Y-%m-%d")
+
+        querystring = {"inboundpartialdate": returnDate.strftime("%Y-%m-%d")}
+        
+        headers = {
+            'x-rapidapi-key': "1e1bf49c20mshbe6efec48c3580dp19cf0djsn75e54d6d7ad2",
+            'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+        }
+
+        result = requests.request("GET", url, headers=headers, params=querystring)
         """getFlights Controller
 
         Raises:
---
+++ backend/src/services/FlightService.py
@@ -0,0 +1,2 @@
+class FlightService:
+    pass
